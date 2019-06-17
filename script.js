e38b2f438ac9de3bec90d124c5cc9ff69aff508b
rahmatrhd <kickyou44@gmail.com>
2019-06-17 21:22:42 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function isPrime (number) {
+  // Your code here
+}
+
+// TEST CASES
+console.log(isPrime(3)); // true
+console.log(isPrime(7)); // true
+console.log(isPrime(6)); // false
+console.log(isPrime(23)); // true
+console.log(isPrime(33)); // false
