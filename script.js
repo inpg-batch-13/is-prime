eb435c0b61a9410ebf24a0913ad98cffc77daa89
Rusydy <rusydymuhiddin@gmail.com>
2019-06-19 20:08:04 +0700

    selesai

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function isPrime (number) {
-  // Your code here
+  if (number === 1) {
+    return false
+  } else if(number === 2) {
+    return true
+  } else {
+    for ( var i = 2; i < number; i++) {
+      if ( number % i === 0) 
+      {
+        return false
+      }
+    }
+  } return true;
 }
 
 // TEST CASES
